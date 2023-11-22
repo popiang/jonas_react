ab76a1c5f2e2acd309cf93569d7e6254202efb8d
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-11-23 07:27:56 +0800

    asdf

--- challenge3/tip-calculator/src/App.js
+++ challenge3/tip-calculator/src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [bill, setBill] = useState(0);
+    const [yourPercent, setYourPercent] = useState(0);
+    const [yourFriendPercent, setYourFriendPercent] = useState(0);
+
+    const handleResetClick = () => {
+        setBill(0);
+        setYourPercent(0);
+        setYourFriendPercent(0);
+    };
+
+    return (
+        <div>
+            <Bill onSetBill={setBill} bill={bill} />
+            <SelectPercentage
+                onSetPercent={setYourPercent}
+                percent={yourPercent}
+            >
+                How did you like the service?
+            </SelectPercentage>
+            <SelectPercentage
+                onSetPercent={setYourFriendPercent}
+                percent={yourFriendPercent}
+            >
+                How did your friend like the service?
+            </SelectPercentage>
+            {bill > 0 && (
+                <>
+                    <Tip
+                        bill={bill}
+                        yourPercent={yourPercent}
+                        yourFriendPercent={yourFriendPercent}
+                    />
+                    <Button onHandleClick={handleResetClick}>Reset</Button>
+                </>
+            )}
+        </div>
+    );
+}
+
+function Bill({ onSetBill, bill }) {
+    return (
+        <div>
+            <span>How much was the bill? </span>
+            <input
+                type="number"
+                onChange={(e) => onSetBill(e.target.value)}
+                value={bill}
+            />
+        </div>
+    );
+}
+
+function SelectPercentage({ onSetPercent, children, percent }) {
+    return (
+        <div>
+            <span>{children}</span>
+            <select
+                onChange={(e) => onSetPercent(e.target.value)}
+                value={percent}
+            >
+                <option value={0}>Dissatisfied (0%)</option>
+                <option value={5}>It was okay (5%)</option>
+                <option value={10}>It was good (10%)</option>
+                <option value={20}>Absolutely amazing (20%)</option>
+            </select>
+        </div>
+    );
+}
+
+function Tip({ bill, yourPercent, yourFriendPercent }) {
+    const totalTip = bill * yourPercent + bill * yourFriendPercent;
+    const totalPay = bill + totalTip;
+
+    return (
+        <div>
+            <h2>{`You pay $${totalPay} ($${bill} + $${totalTip} tip)`}</h2>
+        </div>
+    );
+}
+
+function Button({ children, onHandleClick }) {
+    return <button onClick={onHandleClick}>{children}</button>;
 }
 
 export default App;
