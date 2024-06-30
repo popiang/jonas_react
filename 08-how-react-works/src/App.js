684297fc1c3db597350cbe62899966d81ac5aac2
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-07-01 07:38:37 +0800

    asdf

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -27,7 +27,7 @@ export default function App() {
     );
 }
 
-console.log(<DifferentContent test={23} />);
+// console.log(<DifferentContent test={23} />);
 
 function Tabbed({ content }) {
     const [activeTab, setActiveTab] = useState(0);
@@ -68,9 +68,32 @@ function TabContent({ item }) {
     const [showDetails, setShowDetails] = useState(true);
     const [likes, setLikes] = useState(0);
 
+    console.log("Render");
+
     function handleInc() {
-        setLikes(likes + 1);
+        // setLikes(likes + 1);
+		setLikes(likes => likes + 1); 
     }
+
+    function handleTripleInc() {
+        // setLikes(likes + 1);
+        // setLikes(likes + 1);
+        // setLikes(likes + 1);
+
+        setLikes((likes) => likes + 1);
+        setLikes((likes) => likes + 1);
+        setLikes((likes) => likes + 1);
+    }
+
+    function handleUndo() {
+        setShowDetails(true);
+        setLikes(0);
+        console.log(likes);
+    }
+
+	function handleUndoLater() {
+		setTimeout(handleUndo, 2000);
+	}
 
     return (
         <div className="tab-content">
@@ -85,13 +108,13 @@ function TabContent({ item }) {
                 <div className="hearts-counter">
                     <span>{likes} ❤️</span>
                     <button onClick={handleInc}>+</button>
-                    <button>+++</button>
+                    <button onClick={handleTripleInc}>+++</button>
                 </div>
             </div>
 
             <div className="tab-undo">
-                <button>Undo</button>
-                <button>Undo in 2s</button>
+                <button onClick={handleUndo}>Undo</button>
+                <button onClick={handleUndoLater}>Undo in 2s</button>
             </div>
         </div>
     );
