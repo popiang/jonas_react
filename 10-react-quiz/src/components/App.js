09dc5b6f17a6094ebc35cb5896a2e7e8a89afebe
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-07-18 07:31:37 +0800

    asdf

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -4,11 +4,13 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
     questions: [],
     // loading, error, ready, active, finished
     status: "loading",
+	index: 0
 };
 
 function reducer(state, action) {
@@ -24,13 +26,18 @@ function reducer(state, action) {
                 ...state,
                 status: "error",
             };
+        case "start":
+            return {
+                ...state,
+                status: "active",
+            };
         default:
             throw new Error("Action unknown!!");
     }
 }
 
 export default function App() {
-    const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+    const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
 
@@ -49,8 +56,9 @@ export default function App() {
                 {status === "loading" && <Loader />}
                 {status === "error" && <Error />}
                 {status === "ready" && (
-                    <StartScreen numQuestions={numQuestions} />
+                    <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>
                 )}
+                {status === "active" && <Question question={questions[index]} />}
             </Main>
         </div>
     );
