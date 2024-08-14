2f1d424ec65e48b2eddaf21121e0ce292fa00f89
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-08-15 07:19:53 +0800

    asdf

---
+++ 11-worldwise/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+// pages & components
+import Product from "./pages/Product"
+import Pricing from "./pages/Pricing"
+import Home from "./pages/Home"
+import PageNotFound from "./pages/PageNotFound"
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	)
+}
+
+export default App
---
+++ 11-worldwise/src/pages/Pricing.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Pricing() {
+	return (
+        <div>
+            <PageNav />
+            <h3>Pricing</h3>
+        </div>
+    );
+}
+
+export default Pricing
