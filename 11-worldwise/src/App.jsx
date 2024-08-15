fb510a079677412f4882632c34f9506146109f34
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-08-16 07:24:40 +0800

    worldwise

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // pages & components
-import Product from "./pages/Product"
-import Pricing from "./pages/Pricing"
-import Home from "./pages/Home"
-import PageNotFound from "./pages/PageNotFound"
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="product" element={<Product />} />
-				<Route path="pricing" element={<Pricing />} />
-				<Route path="*" element={<PageNotFound />} />
-			</Routes>
-		</BrowserRouter>
-	)
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="app" element={<AppLayout />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
