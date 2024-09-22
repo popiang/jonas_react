52e977587994b6ea982be6065e8d43c3585b58c0
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-09-22 14:34:02 +0800

    chapter 271 done

---
+++ 15-redux-intro-01/src/features/customers/Customer.js
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+
+function Customer() {
+	const name = useSelector(store => store.customer.fullName);
+
+    return <h2>👋 Welcome, {name}</h2>;
+}
+
+export default Customer;
