dd9c75dbc93dd151a4221908d1156af48b8a140b
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2024-09-29 14:23:03 +0800

    asdf

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -1,10 +1,43 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-	return (
-		<div>
-			Hello Vite!
-		</div>
-	)
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+    {
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/menu",
+                element: <Menu />,
+				loader: menuLoader
+            },
+            {
+                path: "/cart",
+                element: <Cart />,
+            },
+            {
+                path: "/order/:orderId",
+                element: <Order />,
+            },
+            {
+                path: "/order/new",
+                element: <CreateOrder />,
+            },
+        ],
+    },
+]);
+
+function App() {
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
