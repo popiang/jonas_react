fe2c60dcd2d4dbf9a24075b05d3c4b056a3c3f1b
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2024-10-03 16:16:16 +0800

    asdf

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -6,10 +6,12 @@ import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
     {
         element: <AppLayout />,
+        errorElement: <Error />,
         children: [
             {
                 path: "/",
@@ -18,7 +20,8 @@ const router = createBrowserRouter([
             {
                 path: "/menu",
                 element: <Menu />,
-				loader: menuLoader
+                loader: menuLoader,
+                errorElement: <Error />,
             },
             {
                 path: "/cart",
