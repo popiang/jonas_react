2418cf730deea5e378f3b8d602f8d9d5b0b72ec1
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-10-09 00:11:11 +0800

    mengantuk

---
+++ 16-fast-react-pizza/prettier.config.js
@@ -0,0 +1,14 @@
+// prettier.config.js
+const config = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    singleQuote: true,
+    tabWidth: 4,
+};
+
+export default config;
+
+// prettier.config.cjs
+
+// module.exports = {
+//   plugins: ["prettier-plugin-tailwindcss"],
+// }
--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,31 +1,32 @@
-import { useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
     /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-        str
+        str,
     );
 
 const fakeCart = [
     {
         pizzaId: 12,
-        name: "Mediterranean",
+        name: 'Mediterranean',
         quantity: 2,
         unitPrice: 16,
         totalPrice: 32,
     },
     {
         pizzaId: 6,
-        name: "Vegetale",
+        name: 'Vegetale',
         quantity: 1,
         unitPrice: 13,
         totalPrice: 13,
     },
     {
         pizzaId: 11,
-        name: "Spinach and Mushroom",
+        name: 'Spinach and Mushroom',
         quantity: 1,
         unitPrice: 15,
         totalPrice: 15,
@@ -34,8 +35,8 @@ const fakeCart = [
 
 function CreateOrder() {
     const navigation = useNavigation();
-    const isSubmitting = navigation.state === "submitting";
-	const formErrors = useActionData();
+    const isSubmitting = navigation.state === 'submitting';
+    const formErrors = useActionData();
 
     // const [withPriority, setWithPriority] = useState(false);
     const cart = fakeCart;
@@ -47,26 +48,42 @@ function CreateOrder() {
             <Form method="POST">
                 <div>
                     <label>First Name</label>
-                    <input type="text" name="customer" required />
+                    <input
+                        className="input"
+                        type="text"
+                        name="customer"
+                        required
+                    />
                 </div>
 
                 <div>
                     <label>Phone number</label>
                     <div>
-                        <input type="tel" name="phone" required />
+                        <input
+                            className="input"
+                            type="tel"
+                            name="phone"
+                            required
+                        />
                     </div>
-					{formErrors?.phone && <p>{formErrors.phone}</p>}
+                    {formErrors?.phone && <p>{formErrors.phone}</p>}
                 </div>
 
                 <div>
                     <label>Address</label>
                     <div>
-                        <input type="text" name="address" required />
+                        <input
+                            className="input"
+                            type="text"
+                            name="address"
+                            required
+                        />
                     </div>
                 </div>
 
                 <div>
                     <input
+                        className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
                         type="checkbox"
                         name="priority"
                         id="priority"
@@ -84,9 +101,9 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <button disabled={isSubmitting}>
-                        {isSubmitting ? "Placing order..." : "Order now"}
-                    </button>
+                    <Button disabled={isSubmitting} type="primary">
+                        {isSubmitting ? 'Placing order...' : 'Order now'}
+                    </Button>
                 </div>
             </Form>
         </div>
@@ -100,22 +117,24 @@ export async function action({ request }) {
     const order = {
         ...data,
         cart: JSON.parse(data.cart),
-        priority: data.priority === "on",
+        priority: data.priority === 'on',
     };
 
-	const errors = {};
+    const errors = {};
 
-	if (!isValidPhone(order.phone)) {
-		errors.phone = "Invalid phone number";
-	}
+    if (!isValidPhone(order.phone)) {
+        errors.phone = 'Invalid phone number';
+    }
 
-	if (Object.keys(errors).length > 0) {
-		return errors;
-	}
+    if (Object.keys(errors).length > 0) {
+        return errors;
+    }
 
-    const newOrder = await createOrder(order);
+    // const newOrder = await createOrder(order);
 
-    return redirect(`/order/${newOrder.id}`);
+    // return redirect(`/order/${newOrder.id}`);
+
+    return null;
 }
 
 export default CreateOrder;
