9fc80d522e066b676dc5d732ec28209187280ad2
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2024-10-10 17:03:23 +0800

    asdf

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -37,6 +38,7 @@ function CreateOrder() {
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'submitting';
     const formErrors = useActionData();
+    const username = useSelector((state) => state.user.username);
 
     // const [withPriority, setWithPriority] = useState(false);
     const cart = fakeCart;
@@ -54,6 +56,7 @@ function CreateOrder() {
                         className="input grow"
                         type="text"
                         name="customer"
+						defaultValue={username}
                         required
                     />
                 </div>
