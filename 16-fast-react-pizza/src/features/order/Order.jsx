7e249d4ed2955b8e4dd637bedeb2a21447f0832f
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-10-12 23:54:39 +0800

    fast-react-pizza section is done

--- 16-fast-react-pizza/src/features/order/Order.jsx
+++ 16-fast-react-pizza/src/features/order/Order.jsx
@@ -1,16 +1,29 @@
 // Test ID: IIDSAT
 
-import OrderItem from "../order/OrderItem";
-import { useLoaderData } from 'react-router-dom';
+import OrderItem from '../order/OrderItem';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
     calcMinutesLeft,
     formatCurrency,
     formatDate,
 } from '../../utils/helpers';
+import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
     const order = useLoaderData();
+
+    const fetcher = useFetcher();
+
+    useEffect(
+        function () {
+            if (!fetcher.data && fetcher.state === 'idle') {
+                fetcher.load('/menu');
+            }
+        },
+        [fetcher],
+    );
 
     // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
     const {
@@ -54,9 +67,18 @@ function Order() {
                 </p>
             </div>
 
-			<ul className="divide-y divide-stone-200 border-b border-t">
-				{cart.map(item => <OrderItem item={item} key={item.pizzaId} />)}
-			</ul>
+            <ul className="divide-y divide-stone-200 border-b border-t">
+                {cart.map((item) => (
+                    <OrderItem
+                        item={item}
+                        key={item.pizzaId}
+                        isLoadingIngredients={fetcher.state === 'loading'}
+                        ingredients={
+                            fetcher.data?.find((el) => el.id === item.pizzaId)?.ingredients ?? []
+                        }
+                    />
+                ))}
+            </ul>
 
             <div className="space-y-2 bg-stone-200 px-6 py-5">
                 <p className="text-sm font-medium text-stone-600">
@@ -67,15 +89,18 @@ function Order() {
                         Price priority: {formatCurrency(priorityPrice)}
                     </p>
                 )}
-                <p className='font-bold'>
+                <p className="font-bold">
                     To pay on delivery:{' '}
                     {formatCurrency(orderPrice + priorityPrice)}
                 </p>
             </div>
+
+			{!priority && <UpdateOrder order={order} />}
         </div>
     );
 }
 
+// eslint-disable-next-line
 export async function loader({ params }) {
     const order = await getOrder(params.orderId);
     return order;
